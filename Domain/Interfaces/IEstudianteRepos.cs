b4622ff09b1b64fa96686a1fac5fa7e8303c4c01
Steffania Salgado <86125323+SalgadoSteffania@users.noreply.github.com>
2021-12-02 08:42:08 -0600

    Add files via upload

---
+++ Domain/Interfaces/IEstudianteRepos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface IEstudianteRepos: IModel<Estudiante>
+    {
+        List<Estudiante> FindAll(Predicate<Estudiante> predicate);
+        Estudiante GetEstudianteById(int id);
+        List<Estudiante> MejoresPromedios(List<Estudiante> estudiante);
+    }
+}
---
+++ Domain/Interfaces/INotasRepos.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Interfaces
+{
+    public interface INotasRepos : IModel<Nota>
+    {
+        decimal CalculoPromedioNotas(List<Nota> Nota);
+        void Limpiar();
+    }
+}
